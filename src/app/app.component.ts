7d171bac71598e72b1ec8b85ad599b844e49912d
obed vega <vega.obed@gmail.com>
2023-01-25 21:58:55 -0800

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import mdeData from './mde.json';  
+
+interface Mde {  
+  idCliente: String; 
+  geolocalizacion: String;  
+  punto_caliente: String;  
+  cuadrante: String;  
+  fecha: String;
+}  
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myApp';
+  mde: Mde[] = mdeData;  
+  //constructor(){
+  //  console.log(this.mde);
+  //}
+  ImagePath: string;
+  constructor(private http: HttpClient){
+    this.ImagePath = 'favicon.ico'
+    this.loadMdeData();
+    this.loadMdiData();
+  }
+  mdeData: any[] = [];
+  loadMdeData(){
+    this.http
+    .get('https://spysel-monitoreo.herokuapp.com/api/mde')
+    .subscribe((mdeData: any)=>{
+      this.mdeData = mdeData;
+      //console.log(response);
+      //alert(JSON.stringify(response));
+    });
+  }
+  mdiData: any[] = [];
+  loadMdiData(){
+    this.http
+    .get('https://spysel-monitoreo.herokuapp.com/api/mdi')
+    .subscribe((mdiData: any)=>{
+      this.mdiData = mdiData;
+    });
+  }
 }
